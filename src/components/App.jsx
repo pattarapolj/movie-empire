4d8540ca45daabc7b79855d4bc5676d54cad5d7c
pattarapolj <106107329+pattarapolj@users.noreply.github.com>
2025-02-14 03:43:06 +0700

    initialize styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import { CssBaseline } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 
+import useStyles from "./styles";
+
 import { Actors, MovieInformation, Movies, Profile, NavBar } from "./";
 
 const App = () => {
+  const classes = useStyles();
+
+
+
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
       <NavBar />
-      <main>
+      <main className={classes.content}>
+      <div className={classes.toolbar} />
         <Routes>
           <Route path="/movie/:id" exact element={<MovieInformation />} />
           <Route path="/actors/:id" exact element={<Actors />} />
