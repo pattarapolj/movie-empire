97a5f76922206f079b4c2eb41d2a522590d17abb
pattarapolj <106107329+pattarapolj@users.noreply.github.com>
2025-02-15 23:31:45 +0700

    adding NavBar component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import App from "./components/App";
 
+const theme = createTheme({});
+
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    {/* <StrictMode> */}
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      {/* <StrictMode> */}
       <App />
-    {/* </StrictMode> */}
-  </BrowserRouter>
+      {/* </StrictMode> */}
+    </BrowserRouter>
+  </ThemeProvider>
 );
