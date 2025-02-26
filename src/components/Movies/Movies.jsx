11a00779686443d6b0040df5d0e083e9432273e0
pattarapolj <106107329+pattarapolj@users.noreply.github.com>
2025-02-27 03:11:51 +0700

    fetching data on client usign rtk

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import {
+    Box,
+    CircularProgress,
+    useMediaQuery,
+    Typography,
+} from "@mui/material";
+import { useSelector } from "react-redux";
+
+import { useGetMoviesQuery } from "../../services/TMDB";
 
 const Movies = () => {
-    console.log("Movies")
-  return (
-    <div>Movies</div>
-  )
-}
+    const { data } = useGetMoviesQuery();
+    console.log(data);
 
-export default Movies
+    return <div>Movies</div>;
+};
+
+export default Movies;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,22 @@
-// import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
 
 import App from "./components/App";
+import store from "./app/store";
 
 const theme = createTheme({});
 
 createRoot(document.getElementById("root")).render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      {/* <StrictMode> */}
-      <App />
-      {/* </StrictMode> */}
-    </BrowserRouter>
-  </ThemeProvider>
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <StrictMode>
+                    <App />
+                </StrictMode>
+            </BrowserRouter>
+        </ThemeProvider>
+    </Provider>
 );
